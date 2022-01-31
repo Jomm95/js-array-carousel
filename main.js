0b62e9618029f3c3c644d927aa66c1241d0906f7
Thomas <venturini.thomas95@gmail.com>
2022-01-31 01:14:18 +0100

    da aggiungere gli event listener

--- main.js
+++ main.js
@@ -21,3 +21,33 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+
+slides ="";
+
+for ( let i = 0; i < items.length; i++) {
+
+    //ciclo for per creare l'html da mettere nel container
+    slides += ` <div class="main-item">
+                    <img src="${items[i]}">
+                    <div class="img-info">
+                        <h2> ${title[i]} </h2>
+                        <p> ${text[i]} </p>
+                    </div>
+                    
+                </div> ` ;
+}
+
+//creo una variabile che contenga tutti gli items
+let mainSlide = document.getElementById("main-slide");
+
+//metto la variabile contenente tutto nel container
+mainSlide.innerHTML = slides; //non sono visibili perchè non c'è classe "active"
+
+//creo una variabile che traccia la slide attuale
+actualSlide = 0;
+
+//creo un array delle slides per aggiungere la classe active
+let allItem = document.getElementsByClassName("main-item");
+
+allItem[actualSlide].classList.add("active");
